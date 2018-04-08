a44ebca731f1fbad812b0a448777e261dcefb708
Ivana Konatar <ivana.konatar@udg.edu.me>
2018-04-08 19:09:01 +0200

    Homework-task2

--- task2.py
+++ task2.py
@@ -11,3 +11,23 @@
 * Use main() function to test your solution.
 ===================================================
 """
+
+def gcd(a,b):   #f-ja racuna najveci zajednicki djelilac dva cijela broja
+
+    if not isinstance(a,int) and not isinstance(b,int): #ispitujemo da li su unijeti brojevi iz skupa cijelih brojeva
+        return -1
+
+    a1 = abs(a)
+    b1 = abs(b)
+    z = abs(a1-b1)
+
+    if z == 0:      #primjenjujemo Euklidov algoritam
+        return b1
+    else:
+        return gcd(z,min(a1,b1))
+
+def main():
+
+    print(gcd(-120, 24))
+
+main()
